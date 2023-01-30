d977dc6f022b1a61d5e19d9d039f33331af33ff0
Anant Hinge <ananthinge135@gmail.com>
2023-01-30 14:13:37 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ReverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Original array:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + "");
+            }
+            Array.Reverse(numbers);
+
+            Console.WriteLine("\nReversed array:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + "");
+            }
+        }
+    }
+}
